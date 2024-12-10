01fbe1aa4ee9d012d8ebbf501fa13889d4fdcd79
DEV_WOONG <ywcsuwon@gmail.com>
2024-12-10 23:52:27 +0900

    feat: AppConfig, main 연결

--- src/main/java/racingcar/config/AppConfig.java
+++ src/main/java/racingcar/config/AppConfig.java
@@ -1,4 +1,25 @@
 package racingcar.config;
 
+import racingcar.controller.RacingController;
+import racingcar.service.RacingService;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
 public class AppConfig {
+    public RacingController racingController() {
+        return new RacingController(inputView(), outputView(), racingService());
+    }
+
+    private InputView inputView() {
+        return new InputView();
+    }
+
+    private OutputView outputView() {
+        return new OutputView();
+    }
+
+    private RacingService racingService() {
+        return new RacingService();
+    }
+
 }
